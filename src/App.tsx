3be5a96494391f9d2d54087f8b5c0efb8a6758b6
yogesh3434 <yogeshmodgill@gmail.com>
2026-07-24 12:43:21 -0400

    Added initial React+Vite project files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Evonix</h1>
+      <h3>This is Evonix Frontend</h3>
+      <p>All the remaining views and components will be implemented soon</p>
+    </>
+  )
+}
+
+export default App
