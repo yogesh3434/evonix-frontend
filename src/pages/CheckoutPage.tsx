3cc393918b8bb5d8d7564b6cc6b9013009a39a7b
Mrigan Bhalla <mriganbhalla@gmail.com>
2026-08-04 15:50:13 -0400

    Fix CheckoutPage type import

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import type { FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { getCart, removeFromCart } from '../api/cartApi';
 import { formatCurrency } from '../utils/currency';
